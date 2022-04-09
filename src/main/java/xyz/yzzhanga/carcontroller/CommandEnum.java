4f45fe3b2d565943209c3337f2011e48c17bbc34
zhangyingzheng <louis.zhangyz@gmail.com>
2022-04-09 15:00:43 +0800

    调整了工程内容，基本命令写完了

--- src/main/java/xyz/yzzhanga/carcontroller/CommandEnum.java
+++ src/main/java/xyz/yzzhanga/carcontroller/CommandEnum.java
@@ -1,7 +1,17 @@
 package xyz.yzzhanga.carcontroller;
 
 public enum CommandEnum {
-     LEFT("left","a"),RIGHT("right","d"),FORWARD("up","w"),BACKWARD("down","s"),STOP("stop","x");
+    LEFT("left","a"),
+    RIGHT("right","d"),
+        LEFT_DIAGONAL("left_diagonal","q"),
+    RIGHT_DIAGONAL("right_diagonal","e"),
+    LEFT_CONCERNING("left_concerning","r"),
+    RIGHT_CONCERNING("right_concerning","t"),
+    FORWARD("up","w"),
+    BACKWARD("down","s"),
+    right_turnround("right_turnround","g"),
+    turn_of_rear_axis("turn_of_rear_axis","f"),
+    STOP("stop","x");
 
     private final String arg;
     private final String command;
